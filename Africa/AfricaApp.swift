f9831379f510e7ce202c76a5ca4d982496d94046
Volodymyr Mendyk <volodymyrmendyk70@gmail.com>
2023-04-10 15:10:22 +0200

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Volodymyr Mendyk on 10/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
